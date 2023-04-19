4f163812107e186e1bb23464177587c34b3d1550
Newlimited <sbh2043@naver.com>
2023-04-19 12:47:37 +0900

    23.04.19 study

---
+++ first_project/src/main/java/com/yuhan/first_project/service/implement/MainServiceImplement.java
@@ -0,0 +1,19 @@
+
+
+package com.yuhan.first_project.service.implement;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.yuhan.first_project.service.MainService;
+
+@Service
+public class MainServiceImplement  implements MainService{
+
+
+    @Override
+    public String hello() {
+        return "Hello syh";
+    }
+    
+}
