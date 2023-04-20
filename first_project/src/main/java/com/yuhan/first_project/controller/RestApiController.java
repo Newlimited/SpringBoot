5dbbc866c2ca6ebf7f288a38e3e005b329447a1f
Newlimited <sbh2043@naver.com>
2023-04-20 10:49:43 +0900

    4.20 10:50

--- first_project/src/main/java/com/yuhan/first_project/controller/RestApiController.java
+++ first_project/src/main/java/com/yuhan/first_project/controller/RestApiController.java
@@ -1,7 +1,10 @@
 package com.yuhan.first_project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,9 +37,35 @@ public class RestApiController {
     // @RequestMapping (method = RequestMethod.POST, value =" post-method")
     @PostMapping("post-method")
     public String postMethod(){
-        
+
         return "Response of Post Request";
     }
 
-}
+    // Patch Method @PatchMapping
+    // Patch Method : 클라이언트가 서버에 데이터를 일부만 수정하기 위한 요청의 Method
+    @PatchMapping("patch-method")
+    public String patchMethod(){
+        return "Response of Patch Request";
+    }
+    
+    // Delete Method @DeleteMapping
+    // Delete Method : 클라이언트가 서버에 데이터를 삭제하기 위한 요청의 Method
+    @DeleteMapping("delete-method")
+    public String deleteMethod(){
+        return "Response of Delete Request";
+    }
+
+    // PathVariable() 로 Get, Delete Method에서 데이터 받기
+    // 리소스에 지정한 패턴에 맞춰서 요청의 URL을 지정한다면 패턴에 맞춰 데이터를 받아오는 형식
+    @GetMapping({"path-variable/{data1}/{data2}", "path-variable/{data1}"})
+    public String pathVariable(
+        @PathVariable(value = "data1") String dataA,
+        @PathVariable(value = "data2" ,required = false) String dataB
+        // 이것의 입장으로는 입력을 받는것이기 때문에 parameter 자리에 적어준다. 
+    ){ 
+        return dataA +"와" + dataB + "데이터를 입력 받았습니다.";
+    }
+ }
+    
+
 
