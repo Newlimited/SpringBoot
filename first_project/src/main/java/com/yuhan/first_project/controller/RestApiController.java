2842582bceca0b93a07827bcf1d706e1bcc28cdc
Newlimited <sbh2043@naver.com>
2023-04-20 12:06:27 +0900

    23.04.20 12:00

--- first_project/src/main/java/com/yuhan/first_project/controller/RestApiController.java
+++ first_project/src/main/java/com/yuhan/first_project/controller/RestApiController.java
@@ -1,16 +1,43 @@
 package com.yuhan.first_project.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.core.status.Status;
+
+class ParamDto {
+     String data1;
+     String data2;
+
+    public String getData1() {
+        return this.data1;
+    }
+
+    public void setData1(String data1) {
+        this.data1 = data1;
+    }
+
+    public String getData2() {
+        return this.data2;
+    }
+
+    public void setData2(String data2) {
+        this.data2 = data2;
+    }
+
+}
 // Rest API를 위한 Controller 임을 명시해주는 어노테이션 
 // @Controller + @Response Body = @RestController
 // Response는 HTML을 제외한 MIME Type을 반환한다.
@@ -18,9 +45,9 @@ import org.springframework.web.bind.annotation.RestController;
 // URL path 패턴을 지정해서 해당 패턴이면 지정한 클래스로 처리하도록 함
 @RequestMapping(value = "api")
 public class RestApiController {
-    
-    @RequestMapping(method  ={RequestMethod.GET}, value = "hello2")
-    public String hello2(){
+
+    @RequestMapping(method = { RequestMethod.GET }, value = "hello2")
+    public String hello2() {
         return "hello2";
     }
 
@@ -28,15 +55,15 @@ public class RestApiController {
     // GET Method : 클라이언트가 서버에게 데이터를 받기 위한 요청의 Method
     // @RequesetMaaping(method = RequestMethod.GET, value = "get-method")
     @GetMapping("get-method")
-        public String getMethod(){
-            return "Response of Get Request";
-        }
+    public String getMethod() {
+        return "Response of Get Request";
+    }
 
     // POST Method @PostMapping
     // Post Method : 클라이언트가 서버에 데이터를 작성하기 위한 요청의 Method
     // @RequestMapping (method = RequestMethod.POST, value =" post-method")
     @PostMapping("post-method")
-    public String postMethod(){
+    public String postMethod() {
 
         return "Response of Post Request";
     }
@@ -44,28 +71,52 @@ public class RestApiController {
     // Patch Method @PatchMapping
     // Patch Method : 클라이언트가 서버에 데이터를 일부만 수정하기 위한 요청의 Method
     @PatchMapping("patch-method")
-    public String patchMethod(){
+    public String patchMethod() {
         return "Response of Patch Request";
     }
-    
+
     // Delete Method @DeleteMapping
     // Delete Method : 클라이언트가 서버에 데이터를 삭제하기 위한 요청의 Method
     @DeleteMapping("delete-method")
-    public String deleteMethod(){
+    public String deleteMethod() {
         return "Response of Delete Request";
     }
 
     // PathVariable() 로 Get, Delete Method에서 데이터 받기
     // 리소스에 지정한 패턴에 맞춰서 요청의 URL을 지정한다면 패턴에 맞춰 데이터를 받아오는 형식
-    @GetMapping({"path-variable/{data1}/{data2}", "path-variable/{data1}"})
+    @GetMapping({ "path-variable/{data1}/{data2}", "path-variable/{data1}" })
     public String pathVariable(
-        @PathVariable(value = "data1") String dataA,
-        @PathVariable(value = "data2" ,required = false) String dataB
-        // 이것의 입장으로는 입력을 받는것이기 때문에 parameter 자리에 적어준다. 
-    ){ 
-        return dataA +"와" + dataB + "데이터를 입력 받았습니다.";
+            @PathVariable(value = "data1") String dataA,
+            @PathVariable(value = "data2", required = false) String dataB
+    // 이것의 입장으로는 입력을 받는것이기 때문에 parameter 자리에 적어준다.
+    ) {
+        return dataA + "와" + dataB + "데이터를 입력 받았습니다.";
     }
- }
-    
 
+    // @RequestParam 로 GET, Delete MEthod에서 데이터 받기
+    // 완전 path 뒤에 ?name=value[&...] 형식에 맞춰 name에 해당하는 value를 받아오는 형식
+    @GetMapping("request-param")
+    public String requestParam( // ParamDto dto)
+            // path까지만 작성하고 뒤에 따로 받아오는 형태이다
+            // (물음표)? 이후부터는 데이터의 포맷으로 인식한다.
+            @RequestParam String data1,
+            @RequestParam String data2
 
+    ) {
+        // return dto.getData1() + dto.getData2() + "데이터를 입력 받았습니다.";
+        return data1 + data2 + "데이터를 입력 받았습니다.";
+    }
+
+    // @RequestBody 로 Post, Put, Patch Method 에서 데이터 받기
+    // Request Body에 있는 데이터를 받기 위한 어노테이션
+    @PostMapping("request-body")
+    public ResponseEntity<ParamDto> requestBody(
+           // @RequestBody String data 문자열로 받을 적엔 이렇게 ..
+    // 기본적으로 true 가 박혀있고 required 형태로... 반환
+    @RequestBody ParamDto dto
+    ) {
+        return ResponseEntity.status(408).body(dto);
+                // return dto.getData1() + dto.getData2() +"데이터를 받았습니다.";
+        // return data + "데이터를 받았습니다.";
+    }
+}
