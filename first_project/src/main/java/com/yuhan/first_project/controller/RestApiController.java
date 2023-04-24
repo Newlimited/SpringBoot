93b3dd0512e6d12e4595e94fb32d90246df52e17
Newlimited <sbh2043@naver.com>
2023-04-24 09:57:34 +0900

    23.04.24 9:50

--- first_project/src/main/java/com/yuhan/first_project/controller/RestApiController.java
+++ first_project/src/main/java/com/yuhan/first_project/controller/RestApiController.java
@@ -1,7 +1,7 @@
 package com.yuhan.first_project.controller;
-
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.StreamingHttpOutputMessage.Body;
@@ -18,9 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.yuhan.first_project.dto.request.ExampleDto;
 import com.yuhan.first_project.dto.response.ExampleResponseDto;
+import com.yuhan.first_project.service.implement.RestApiService;
+import com.yuhan.first_project.service.implement.RestApiServiceImplement;
 
 import ch.qos.logback.core.status.Status;
 
@@ -53,6 +54,19 @@ class ParamDto {
 @RequestMapping(value = "api")
 public class RestApiController {
 
+    // private RestApiServiceImplement restApiServiceImplement = 
+    // new RestApiServiceImplement(); // 의존성을 직접 만들고있음 
+    // Service 에서 하나만 오류가 뜨면 그것만 떠야하는데 이렇게 되어있으면
+    // 하나가 오류나면 전체를 이용하지 못한다. 그래서 Spring IOC를 이용해서 주입해볼것이다.
+    // 그러기 위해선 bean 에 등록해야한다. 방법은 @component 하면된다 (or @Service)
+   
+    // private RestApiServiceImplement restApiService; // spring이 알아서 불러온다 
+    public RestApiService restApiService;
+
+    public RestApiController(RestApiServiceImplement restApiService){
+        this.restApiService = restApiService;
+    }
+
     @RequestMapping(method = { RequestMethod.GET }, value = "hello2")
     public String hello2() {
         return "hello2";
@@ -63,7 +77,7 @@ public class RestApiController {
     // @RequesetMaaping(method = RequestMethod.GET, value = "get-method")
     @GetMapping("get-method")
     public String getMethod() {
-        return "Response of Get Request";
+        return restApiService.getMethod();
     }
 
     // POST Method @PostMapping
@@ -72,21 +86,21 @@ public class RestApiController {
     @PostMapping("post-method")
     public String postMethod() {
 
-        return "Response of Post Request";
+        return restApiService.postMethod();
     }
 
     // Patch Method @PatchMapping
     // Patch Method : 클라이언트가 서버에 데이터를 일부만 수정하기 위한 요청의 Method
     @PatchMapping("patch-method")
     public String patchMethod() {
-        return "Response of Patch Request";
+        return restApiService.patchMethod();
     }
 
     // Delete Method @DeleteMapping
     // Delete Method : 클라이언트가 서버에 데이터를 삭제하기 위한 요청의 Method
     @DeleteMapping("delete-method")
     public String deleteMethod() {
-        return "Response of Delete Request";
+        return restApiService.deleteMethod();
     }
 
     // PathVariable() 로 Get, Delete Method에서 데이터 받기
