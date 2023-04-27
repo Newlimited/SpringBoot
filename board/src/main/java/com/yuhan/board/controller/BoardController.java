64f6b650a4c29eeada146971231ad2ac08a73b42
Newlimited <sbh2043@naver.com>
2023-04-27 11:01:15 +0900

    23.04.27 11:00

--- board/src/main/java/com/yuhan/board/controller/BoardController.java
+++ board/src/main/java/com/yuhan/board/controller/BoardController.java
@@ -1,8 +1,35 @@
 package com.yuhan.board.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yuhan.board.dto.request.Board.PostBoardRequestDto;
+import com.yuhan.board.dto.response.ResponseDto;
+import com.yuhan.board.service.BoardService;
+
 @RestController
+@RequestMapping("/api/v1/board")
 public class BoardController {
-    
+    private BoardService boardService;
+    @Autowired
+    public BoardController(BoardService boardService){
+        this.boardService = boardService;
+    }
+
+    // 1. 게시물 작성
+
+    @PostMapping("")//추가적인 path 없음
+    public ResponseEntity<ResponseDto> postBoard(
+       @Valid @RequestBody PostBoardRequestDto requestBody
+    ){
+        ResponseEntity<ResponseDto> response= boardService.postBoard(requestBody);
+        return response;
+    }
+
 }
--- board/src/main/java/com/yuhan/board/service/implement/BoardServiceImplement.java
+++ board/src/main/java/com/yuhan/board/service/implement/BoardServiceImplement.java
@@ -1,10 +1,55 @@
 package com.yuhan.board.service.implement;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.yuhan.board.dto.request.Board.PatchBoardRequestDto;
+import com.yuhan.board.dto.request.Board.PostBoardRequestDto;
+import com.yuhan.board.dto.response.ResponseDto;
+import com.yuhan.board.dto.response.board.GetBoardListResponseDto;
+import com.yuhan.board.dto.response.board.GetBoardResponseDto;
 import com.yuhan.board.service.BoardService;
 
 @Service
 public class BoardServiceImplement implements BoardService {
+
+    @Override
+    public ResponseEntity<ResponseDto> postBoard(PostBoardRequestDto dto) {
+       
+        throw new UnsupportedOperationException("Unimplemented method 'postBoard'");
+    }
+
+    @Override //<? super GetBoardResponseDto> 와일드 카드의 하한을 제한. GetBoardResponseDto와 그 조상들만가능
+    // 반환하는 타입이 반환되는 타입이 다르다. 
+    // 성공되면 GetBoardResponseDto 클래스의 반환타입으로 반환될거고 
+    // 실패하면 code, message 형태인 조상클래스의 반환타입으로 반환될것이다.
+    public ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'getBoard'");
+    }
+
+    @Override
+    public ResponseEntity<? super GetBoardListResponseDto> getBoardList() {
+      
+        throw new UnsupportedOperationException("Unimplemented method 'getBoardList'");
+    }
+
+    @Override
+    public ResponseEntity<? super GetBoardListResponseDto> getBoardTop3() {
+     
+        throw new UnsupportedOperationException("Unimplemented method 'getBoardTop3'");
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> patchBoard(PatchBoardRequestDto dto) {
+     
+        throw new UnsupportedOperationException("Unimplemented method 'patchBoard'");
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> deleteBoard(String userEmail, Integer boardNumber) {
+     
+        throw new UnsupportedOperationException("Unimplemented method 'deleteBoard'");
+    }
     
 }
